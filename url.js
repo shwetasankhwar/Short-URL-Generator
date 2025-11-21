4fcc99118ac875c66b04c2d7a9ab5e59351d195d
shweta <110660376+shwetasankhwar@users.noreply.github.com>
2025-11-21 15:19:48 +0530

    Add files via upload
    controller url

---
+++ url.js
@@ -0,0 +1,43 @@
+const shortid = require('shortid');
+
+const URL = require('../models/url');
+
+async function handleGenerateNewShortURL(req,res){
+const body = req.body;
+if(!body.url) {
+    return res.status(400).json({error:"url is required"})
+}
+const shortID = shortid(); 
+
+await URL.create({
+    shortID: shortID,
+    redirectURL: body.url,
+    visitHistory: [],
+    createdBy: req.user._id,
+    
+
+
+})
+console.log("Short URL saved:", shortID);
+
+return res.render('home',{ 
+    id : shortID,
+})
+
+}
+
+
+async function handleGetAnalytic(req,res){
+    const shortID = req.params.shortID;
+    const result = await URL.findOne({shortID})
+    return res.json({
+        totalClicks: result.visitHistory.length,
+        analytics: result.visitHistory,})
+}
+
+module.exports = {
+    handleGenerateNewShortURL,
+    handleGetAnalytic,
+}
+
+
